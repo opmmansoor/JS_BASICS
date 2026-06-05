4564a2543904f011c9d538a73e057128a5f24478
Muhammed Mansoor <mansoor@email.com>
2026-06-05 15:54:06 +0530

    String Search Methods and Template Literals

---
+++ day.2.2.js
@@ -0,0 +1,63 @@
+//String Search Methods
+
+
+//1. indexOf()  [Returns the first position (index) of a specified value]
+let text = "Hello World";
+console.log(text.indexOf("World"));
+console.log(text.indexOf("Java"));
+
+
+//2. lastIndexOf() [Returns the last occurrence of a value.]
+let texts = "what is th is";
+console.log(texts.lastIndexOf("is"));
+
+
+//3. search()  [Searches for a value and returns its position.]
+let call = "Happy Day";
+console.log(call.search('Happy'));
+
+
+// 4. match() [ Returns matched values from a string]
+let message = "I love JavaScript";
+console.log(message.match("Script"));
+
+
+/*5.includes() [Checks whether a string contains a value.
+Returns: true or false   ]*/
+let type = "Happy World";
+console.log(type.includes("World"));
+
+
+/*6.startsWith()  [Checks whether a string starts with a specified value.
+Returns: true or false ] */
+let tip = "JavaScript";
+console.log(tip.startsWith("Java"));
+console.log(tip.startsWith("Script"));
+
+
+//7. endsWith() [Checks whether a string ends with a specified value.]
+let tips = "JavaScript";
+console.log(tips.endsWith("Script"));
+
+
+
+
+/*topic ..... 3
+
+Template Literals
+(`  `) backticks      ${} Interpolation
+Template Literals are a way to create strings using backticks (` `) instead of single quotes (' ') or double quotes (" "). */ 
+
+let name = "Mansoor";
+let age = 20;
+console.log(`My name is ${name} and I am ${age} years old`);
+
+//CALCULATION
+let a = 10;
+let b = 20;
+console.log(`Sum = ${a + b}`);
+
+//Multi-line Strings
+let item = `Hello
+Welcome`;
+console.log(item);
