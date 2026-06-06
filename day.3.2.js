2fd19338e304b97ca93bc0b382363b529e4a72b1
Muhammed Mansoor <mansoor@email.com>
2026-06-06 17:37:29 +0530

    ARRAY Search Method

---
+++ day.3.2.js
@@ -0,0 +1,31 @@
+//Array Search Methods
+//1. indexOf()
+let fruits = ["Apple","Mango","Orange", "Mango"];
+console.log(fruits.indexOf("Mango"));
+
+
+//2. lastIndexOf() [ lastIndexOf() returns the last index (position) of a specified value in an array.
+//If the value is not found, it returns -1. ]
+let numbers = [10, 20, 30, 20];
+console.log(numbers.lastIndexOf(20));
+
+
+//3. includes()  [ checks whether a value exists in an array.
+// true  or false ]
+let fruit = ["Apple", "Orange", "Mango"];
+console.log(fruit.includes("Mango"));
+
+
+//4.find() [returns the first element in an array that matches a condition.]
+//If no element matches the condition, it returns undefined.
+//array.find(function)
+let number = [10, 20, 30, 40];
+let result = number.find(num => num > 10);
+console.log(result);
+
+
+//5.findIndex() [ returns the index (position) of the first element that matches a condition.
+//If no element matches the condition, it returns -1. ]
+let nums = [10, 20, 30, 40];
+let results = nums.findIndex(num => num > 20);
+console.log(results);
